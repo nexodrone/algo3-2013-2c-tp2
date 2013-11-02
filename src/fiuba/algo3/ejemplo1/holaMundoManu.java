aeab0f2eb6321f1627c34e16ee928d246774ce8d
m.iglesias91 <m.iglesias91@gmail.com>
2013-11-02 20:41:04 +0000

    Subo la clase "holaMundoManu" que imprime "Hola Mundo Manuel" cuando se abre. Tambien cree un test para la clase, que testea el método "saludar".

---
+++ src/fiuba/algo3/ejemplo1/holaMundoManu.java
@@ -0,0 +1,13 @@
+package fiuba.algo3.ejemplo1;
+
+public class holaMundoManu {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo Manuel");
+	}
+	
+	public String saludar(){
+		return "Buenas";
+	}
+
+}
