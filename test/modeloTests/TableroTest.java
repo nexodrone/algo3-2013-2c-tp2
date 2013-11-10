6845e6f3131b0905e8e6cbb71df583212ced4141
cabrerajjorge <cabrerajjorge@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-11-10 00:57:07 +0000

    Jorge Cabrera: Modificaciones en tablero, juegoTest y tableroTest antes estaba en conflicto

--- test/modeloTests/TableroTest.java
+++ test/modeloTests/TableroTest.java
@@ -1,6 +1,8 @@
 package modeloTests;
 
 import static org.junit.Assert.*;
+import modelo.Bocacalle;
+import modelo.Posicion;
 import modelo.Tablero;
 import org.junit.Test;
 
@@ -13,10 +15,18 @@ public class TableroTest {
 	}
 
 	@Test
+	public void testObtenerBocacalleATransitarDeUnaPosicionValidaNoDeberiaSerNula(){
+		Tablero unTablero = new Tablero(10,10);
+		Bocacalle unaBocacalle = unTablero.bocacalleDeReferencia(new Posicion(0,0));
+		assertNotNull(unaBocacalle);
+	}
+
+	@Test
 	public void testCalleEntreDosBocacallesAdyacentesDeberiaSerMismoObjeto() {
 		Tablero unTablero = new Tablero(5,5);
 		assertEquals(unTablero.getBocacalleEnPosicion(0,0).obtenerCalleEnDireccion('E'),unTablero.getBocacalleEnPosicion(0,1).obtenerCalleEnDireccion('O'));
 		assertEquals(unTablero.getBocacalleEnPosicion(4,4).obtenerCalleEnDireccion('N'),unTablero.getBocacalleEnPosicion(3,4).obtenerCalleEnDireccion('S'));
 		assertEquals(unTablero.getBocacalleEnPosicion(2,2).obtenerCalleEnDireccion('S'),unTablero.getBocacalleEnPosicion(3,2).obtenerCalleEnDireccion('N'));
 	}
+
 }
