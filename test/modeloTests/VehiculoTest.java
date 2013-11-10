1ed5cacd0a5a0ba6d078c351fe7a1f69a9730038
cabrerajjorge <cabrerajjorge@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-11-10 00:36:41 +0000

    JorgeCabrera: modifique la forma en que se mueve el vehiculo, ahora el vehiculo conoce la bocacalle y se mueve a través de ésta, ya no necesita que el tablero obtenga de la bocacalle, la calle y se la envié al vehículo. Agregué la Clase partida.

--- test/modeloTests/VehiculoTest.java
+++ test/modeloTests/VehiculoTest.java
@@ -3,6 +3,7 @@ package modeloTests;
 import static org.junit.Assert.*;
 import modelo.Vehiculo;
 import modelo.Posicion;
+import modelo.Bocacalle;
 import org.junit.Test;
 
 public class VehiculoTest {
@@ -11,5 +12,15 @@ public class VehiculoTest {
 	public void testDeberiaCrearVehiculo(){
 		Vehiculo unVehiculo = new Vehiculo(new Posicion(0,0));
 		assertNotNull(unVehiculo);
+		
 	}
+	
+	
+	public void testVehiculoUbicadoEnUnaBocacalleNoDeberiaTenerBocacalleNula(){
+		Vehiculo unVehiculo = new Vehiculo(new Posicion(0,0));
+		Bocacalle unaBocacalle = new Bocacalle();
+		unVehiculo.setBocacalle(unaBocacalle);
+		assertNotNull(unVehiculo.getBocacalle());
+	}
+	
 }
