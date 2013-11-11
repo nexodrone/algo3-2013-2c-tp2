7b68f26ec0da69242ed9d9e88dfc7eb8542db4c8
cristian3629 <cristian3629@gmail.com>
2013-11-11 18:31:49 +0000

    agregue copy() sumarColumna() sumarFila()

--- src/modelo/Posicion.java
+++ src/modelo/Posicion.java
@@ -2,34 +2,46 @@ package modelo;
 
 public class Posicion {
 
-	private int fila,columna;
+    private int fila, columna;
 
-	public Posicion(int fila, int columna) {
-		this.fila = fila;
-		this.columna = columna;
-	}
+    public Posicion(int fila, int columna) {
+        this.fila = fila;
+        this.columna = columna;
+    }
 
-	public void setFila(int fila) {
-		this.fila = fila;
-	}
+    public void setFila(int fila) {
+        this.fila = fila;
+    }
 
-	public void setColumna(int columna) {
-		this.columna = columna;
-	}
+    public void setColumna(int columna) {
+        this.columna = columna;
+    }
 
-	public int getFila() {
-		return fila;
-	}
+    public int getFila() {
+        return fila;
+    }
 
-	public int getColumna() {
-		return columna;
-	}
+    public int getColumna() {
+        return columna;
+    }
 
-	public String asString() {
-		String stringFila, stringColumna;
-		stringFila = String.valueOf(this.fila);
-		stringColumna = String.valueOf(this.columna);
-		return stringFila + "," + stringColumna;
-	}
+    public String asString() {
+        String stringFila, stringColumna;
+        stringFila = String.valueOf(this.fila);
+        stringColumna = String.valueOf(this.columna);
+        return stringFila + "," + stringColumna;
+    }
 
+    public Posicion copy() {
+        Posicion nuevaPosicion = new Posicion(fila, columna);
+        return nuevaPosicion;
+    }
+
+    public void sumarFila(int filaASumar) {
+        fila = fila + filaASumar;
+    }
+
+    public void sumarColumna(int columnaASumar) {
+        columna = columna + columnaASumar;
+    }
 }
