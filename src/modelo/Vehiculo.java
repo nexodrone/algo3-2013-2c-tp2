f41fe34fe58e766d5e4d47bd237cf87abd5f8eda
cristian3629@gmail.com <cristian3629@gmail.com@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-11-14 16:46:14 +0000

    arreglando conflicto, agregue metodo cambiarA(nuevoVehiculo)

--- src/modelo/Vehiculo.java
+++ src/modelo/Vehiculo.java
@@ -2,13 +2,17 @@ package modelo;
 
 import excepciones.ImposiblePasarPorCalleException;
 import excepciones.MovimientoNoRealizadoException;
-import modelo.Calle;
-
 
 public abstract class Vehiculo {
 
     protected Vector posicion;
     protected int cantidadDeMovimientos;
+    protected Juego juego;
+
+    public Vehiculo(Vector nuevaPosicion, int movimientos) {
+        posicion = nuevaPosicion;
+        cantidadDeMovimientos = movimientos;
+    }
 
     public Vector getPosicion() {
         return this.posicion;
@@ -18,19 +22,17 @@ public abstract class Vehiculo {
         return this.cantidadDeMovimientos;
     }
 
-    public void moverEnDireccion(Vector direccion, Calle calleAPasar)
-    		throws MovimientoNoRealizadoException
-    {
-    	// Aca supuestamente pasa por calle (si puede),
-    	// pero necesita la bocacalle. Como ahora no conoce a tablero,
-    	// lo va a tener que pedir al juego
-    	try { 
-    		pasarPorCalle(calleAPasar);
-    	}catch(ImposiblePasarPorCalleException e) {
-    		throw new MovimientoNoRealizadoException();
-    	}
-    	this.cantidadDeMovimientos = cantidadDeMovimientos + 1;
-    	this.posicion = calcularSiguientePosicion(direccion);
+    public void moverEnDireccion(Vector direccion, Calle calleAPasar) throws MovimientoNoRealizadoException {
+        // Aca supuestamente pasa por calle (si puede),
+        // pero necesita la bocacalle. Como ahora no conoce a tablero,
+        // lo va a tener que pedir al juego
+        try {
+            pasarPorCalle(calleAPasar);
+        } catch (ImposiblePasarPorCalleException e) {
+            throw new MovimientoNoRealizadoException();
+        }
+        this.cantidadDeMovimientos = cantidadDeMovimientos + 1;
+        this.posicion = calcularSiguientePosicion(direccion);
     }
 
     public Vector calcularSiguientePosicion(Vector direccion) {
@@ -52,8 +54,18 @@ public abstract class Vehiculo {
     }
 
     public void cambiarA(Vehiculo vehiculo) {
-        //Juego.cambiarVehiculo(vehiculo);
+        juego.cambiarVehiculo(vehiculo);
     }
 
-    
+    public boolean tienenElMismoEstado(Vehiculo vehiculo) {
+        boolean cantidad = (cantidadDeMovimientos == vehiculo.getCantidadDeMovimientos());
+        boolean posicionesIguales = posicion.sonIguales(vehiculo.getPosicion());
+        return (cantidad && posicionesIguales);
+
+    }
+
+    public void setJuegoActual(Juego juegoActual) {
+        juego = juegoActual;
+    }
+
 }
