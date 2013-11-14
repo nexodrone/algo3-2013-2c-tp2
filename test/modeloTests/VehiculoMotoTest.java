8b1d7d4face77b1665c239c0336a7c47fd2dc3ca
cristian3629 <cristian3629@gmail.com>
2013-11-14 16:48:14 +0000

    arreglando conflicto, agregue metodo cambiarA(nuevoVehiculo)

--- test/modeloTests/VehiculoMotoTest.java
+++ test/modeloTests/VehiculoMotoTest.java
@@ -1,23 +1,170 @@
 package modeloTests;
 
 import static org.junit.Assert.assertEquals;
-
-import modelo.ObstaculoPiquete;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import modelo.Calle;
+import modelo.Juego;
+import modelo.ObstaculoPiquete;
+import modelo.Sorpresa;
+import modelo.SorpresaCambioDeVehiculo;
+import modelo.SorpresaDesfavorable;
+import modelo.SorpresaFavorable;
 import modelo.Vector;
+import modelo.Vehiculo;
+import modelo.Vehiculo4x4;
+import modelo.VehiculoAuto;
 import modelo.VehiculoMoto;
 
 import org.junit.Test;
 
 public class VehiculoMotoTest {
 
-		@Test
-		public void testDeberiaPerderDosMovimientosPorPiquete() {
-			VehiculoMoto moto = new VehiculoMoto(new Vector(2,2), 0);
-			Calle calleATransitar = new Calle(new ObstaculoPiquete(), null);
-			
-			moto.pasarPorCalle(calleATransitar);
-			assertEquals(2, moto.getCantidadDeMovimientos());
-		}
+    @Test
+    public void testDeberiaPerderDosMovimientosPorPiquete() {
+        VehiculoMoto moto = new VehiculoMoto(new Vector(2, 2), 0);
+        Calle calleATransitar = new Calle(new ObstaculoPiquete(), null);
+
+        moto.pasarPorCalle(calleATransitar);
+        assertEquals(2, moto.getCantidadDeMovimientos());
+    }
+
+    // TESTS DE METODOS
+    @Test
+    public void testParaComprobarQueDosVehiculosTieneElMismoEstadoFuncionaCorrectamente() {
+        Vector posicion = new Vector(1, 2);
+        int cantidad = 100;
+        VehiculoMoto unVehiculo = new VehiculoMoto(posicion, cantidad);
+        VehiculoMoto otroVehiculo = new VehiculoMoto(posicion, cantidad);
+        assertTrue(unVehiculo.tienenElMismoEstado(otroVehiculo));
+    }
+
+    @Test
+    public void testParaComprobarQueElMismoVehiculoTieneElMismoEstado() {
+        Vector posicion = new Vector(1, 2);
+        int cantidad = 100;
+        VehiculoMoto unVehiculo = new VehiculoMoto(posicion, cantidad);
+        assertTrue(unVehiculo.tienenElMismoEstado(unVehiculo));
+    }
+
+    @Test
+    public void testParaComprobarQueCuandoTieneDistintosEstados() {
+        // Si tienen distintas cantidad de movimientos, son distintos estados
+        Vector posicion = new Vector(1, 2);
+        int cantidad = 100;
+        int otraCantidad = 120;
+        VehiculoMoto unVehiculo = new VehiculoMoto(posicion, cantidad);
+        Vehiculo otroVehiculo = new VehiculoMoto(posicion, otraCantidad);
+
+        assertFalse(unVehiculo.tienenElMismoEstado(otroVehiculo));
+
+    }
+
+    @Test
+    public void testParaComprobarQueDevuelveUnaInstanciaConElMismoEstadoDeLaMoto() {
+        Vector posicion = new Vector(1, 2);
+        int cantidad = 100;
+        Vehiculo unVehiculo = new VehiculoMoto(posicion, cantidad);
+        Vehiculo otroVehiculo = VehiculoMoto.nuevoVehiculo(unVehiculo);
+
+        assertTrue(unVehiculo.tienenElMismoEstado(otroVehiculo));
+    }
+
+    @Test
+    public void testParaComprobarQueDevuelveUnaInstanciaConElMismoEstadoDelAuto() {
+        Vector posicion = new Vector(1, 2);
+        int cantidad = 100;
+        Vehiculo unVehiculo = new VehiculoAuto(posicion, cantidad);
+        Vehiculo otroVehiculo = VehiculoMoto.nuevoVehiculo(unVehiculo);
+
+        assertTrue(unVehiculo.tienenElMismoEstado(otroVehiculo));
+    }
+
+    @Test
+    public void testParaComprobarQueDevuelveUnaInstanciaConElMismoEstadoDel4x4() {
+        Vector posicion = new Vector(1, 2);
+        int cantidad = 100;
+        Vehiculo unVehiculo = new Vehiculo4x4(posicion, cantidad);
+        Vehiculo otroVehiculo = VehiculoMoto.nuevoVehiculo(unVehiculo);
+
+        assertTrue(unVehiculo.tienenElMismoEstado(otroVehiculo));
+    }
+
+    // TESTS DE COMPORTAMIENTO SORPRESA-VEHICULOMOTO
+
+    @Test
+    public void testParaComprobarQueCuandoSeLePasaUnaSorpresaFavorableTieneMenosMovimientos() {
+        int cantidadDeMovimientos = 100;
+        VehiculoMoto vehiculo = new VehiculoMoto(null, cantidadDeMovimientos);
+        Sorpresa sorpresa = new SorpresaFavorable();
+
+        vehiculo.aplicarEvento(sorpresa);
+        // se resta el 20%
+        int nuevaCantidad = (int) (cantidadDeMovimientos - ((0.2) * cantidadDeMovimientos));
+
+        assertEquals(vehiculo.getCantidadDeMovimientos(), nuevaCantidad);
+    }
+
+    @Test
+    public void testParaComprobarQueCuandoSeLePasaUnaSorpresaDesfavorableTieneMasMovimientos() {
+        int cantidadDeMovimientos = 100;
+        VehiculoMoto vehiculo = new VehiculoMoto(null, cantidadDeMovimientos);
+        Sorpresa sorpresa = new SorpresaDesfavorable();
+
+        vehiculo.aplicarEvento(sorpresa);
+
+        // Aumenta en un 25%
+        int nuevaCantidad = (int) (cantidadDeMovimientos + (0.25) * cantidadDeMovimientos);
+        assertEquals(vehiculo.getCantidadDeMovimientos(), nuevaCantidad);
+    }
+
+    @Test
+    public void testParaComprobarQueCuandoCambiaDeVehiculoNoEsElMismoObjeto() {
+        int cantidadDeMovimientos = 0;
+        VehiculoMoto vehiculo = new VehiculoMoto(null, cantidadDeMovimientos);
+        Sorpresa sorpresa = new SorpresaCambioDeVehiculo();
+        Juego juego = new Juego(null, vehiculo, null);
+
+        vehiculo.setJuegoActual(juego);
+        vehiculo.aplicarEvento(sorpresa);
+
+        Vehiculo nuevoVehiculo = juego.getVehiculo();
+
+        assertFalse(nuevoVehiculo == vehiculo);
+
+    }
+
+    @Test
+    public void testParaComprobarQueCuandoDeVehiculoElMismoTieneElMismoEstadoQueElOtroVehiculo() {
+        int cantidadDeMovimientos = 0;
+        Vector posicion = new Vector(1, 2);
+        VehiculoMoto vehiculo = new VehiculoMoto(posicion, cantidadDeMovimientos);
+        Sorpresa sorpresa = new SorpresaCambioDeVehiculo();
+        Juego juego = new Juego(null, vehiculo, null);
+
+        vehiculo.setJuegoActual(juego);
+        vehiculo.aplicarEvento(sorpresa);
+
+        Vehiculo nuevoVehiculo = juego.getVehiculo();
+
+        assertTrue(vehiculo.tienenElMismoEstado(nuevoVehiculo));
+    }
+
+    @Test
+    public void testParaComprobarQueElNuevoVehiculoEsUnAuto() {
+        int cantidadDeMovimientos = 0;
+        VehiculoMoto vehiculo = new VehiculoMoto(null, cantidadDeMovimientos);
+        Sorpresa sorpresa = new SorpresaCambioDeVehiculo();
+        Juego juego = new Juego(null, vehiculo, null);
+
+        vehiculo.setJuegoActual(juego);
+        vehiculo.aplicarEvento(sorpresa);
+
+        Vehiculo nuevoVehiculo = juego.getVehiculo();
+
+        Vehiculo otroVehiculo = new VehiculoAuto(null, 0);
+
+        assertEquals(nuevoVehiculo.getClass(), otroVehiculo.getClass());
+    }
 
 }
--- test/modeloTests/VehiculoTest.java
+++ test/modeloTests/VehiculoTest.java
@@ -3,15 +3,12 @@ package modeloTests;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import modelo.Bocacalle;
-import modelo.Calle;
-import modelo.ObstaculoPiquete;
 import modelo.Vector;
 import modelo.Vehiculo;
 import modelo.VehiculoAuto;
 
 import org.junit.Test;
 
-import excepciones.ImposiblePasarPorCalleException;
 import excepciones.MovimientoInvalidoExcepcion;
 import excepciones.MovimientoNoRealizadoException;
 
@@ -19,37 +16,50 @@ public class VehiculoTest {
 
     @Test
     public void testDeberiaCrearVehiculo() {
-    	Vehiculo vehiculo = new VehiculoAuto(new Vector(2,2), 0);
+        Vehiculo vehiculo = new VehiculoAuto(new Vector(2, 2), 0);
         assertNotNull(vehiculo);
     }
 
     @Test
     public void testVehiculoDeberiaQuedarseEnPosicionIndicada() {
-    	Vehiculo unVehiculo = new VehiculoAuto(new Vector(2,3), 0);
+        Vehiculo unVehiculo = new VehiculoAuto(new Vector(2, 3), 0);
         assertEquals(unVehiculo.getPosicion().asString(), "2,3");
     }
 
     @Test
     public void testPoderMoverseEnTodasDireccionesPosibles() throws MovimientoInvalidoExcepcion {
-    	Vehiculo unVehiculo = new VehiculoAuto(new Vector(2,3), 0);
-    	Bocacalle bocacalleNeutral = new Bocacalle();
-    	Vector norte = new Vector(0,1);
-    	Vector sur = new Vector(0,-1);
-    	Vector este = new Vector(1,0);
-    	Vector oeste = new Vector(-1,0);
-        try {unVehiculo.moverEnDireccion(sur, bocacalleNeutral.obtenerCalleEnDireccion(sur));}
-        catch(MovimientoNoRealizadoException e) {};
-        try {unVehiculo.moverEnDireccion(este, bocacalleNeutral.obtenerCalleEnDireccion(este));}
-        catch(MovimientoNoRealizadoException e){};
-        try {unVehiculo.moverEnDireccion(sur, bocacalleNeutral.obtenerCalleEnDireccion(sur));}
-        catch(MovimientoNoRealizadoException e){};
-        try {unVehiculo.moverEnDireccion(oeste, bocacalleNeutral.obtenerCalleEnDireccion(oeste));}
-        catch(MovimientoNoRealizadoException e){};
-        try {unVehiculo.moverEnDireccion(oeste, bocacalleNeutral.obtenerCalleEnDireccion(oeste));}
-        catch(MovimientoNoRealizadoException e){};
-        try {unVehiculo.moverEnDireccion(norte, bocacalleNeutral.obtenerCalleEnDireccion(norte));}
-        catch(MovimientoNoRealizadoException e){};
+        Vehiculo unVehiculo = new VehiculoAuto(new Vector(2, 3), 0);
+        Bocacalle bocacalleNeutral = new Bocacalle();
+        Vector norte = new Vector(0, 1);
+        Vector sur = new Vector(0, -1);
+        Vector este = new Vector(1, 0);
+        Vector oeste = new Vector(-1, 0);
+        try {
+            unVehiculo.moverEnDireccion(sur, bocacalleNeutral.obtenerCalleEnDireccion(sur));
+        } catch (MovimientoNoRealizadoException e) {
+        };
+        try {
+            unVehiculo.moverEnDireccion(este, bocacalleNeutral.obtenerCalleEnDireccion(este));
+        } catch (MovimientoNoRealizadoException e) {
+        };
+        try {
+            unVehiculo.moverEnDireccion(sur, bocacalleNeutral.obtenerCalleEnDireccion(sur));
+        } catch (MovimientoNoRealizadoException e) {
+        };
+        try {
+            unVehiculo.moverEnDireccion(oeste, bocacalleNeutral.obtenerCalleEnDireccion(oeste));
+        } catch (MovimientoNoRealizadoException e) {
+        };
+        try {
+            unVehiculo.moverEnDireccion(oeste, bocacalleNeutral.obtenerCalleEnDireccion(oeste));
+        } catch (MovimientoNoRealizadoException e) {
+        };
+        try {
+            unVehiculo.moverEnDireccion(norte, bocacalleNeutral.obtenerCalleEnDireccion(norte));
+        } catch (MovimientoNoRealizadoException e) {
+        };
         assertEquals(unVehiculo.getPosicion().asString(), "1,2");
         assertEquals(unVehiculo.getCantidadDeMovimientos(), 6);
     }
+
 }
