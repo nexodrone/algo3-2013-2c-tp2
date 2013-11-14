f1b63bc4d4aeb57bcaab064bfc800aa61f82e38e
cristian3629@gmail.com <cristian3629@gmail.com@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-11-14 03:34:04 +0000

    actualizando sorpresa y vehiculos, terminando el doble dispatch

--- src/modelo/SorpresaCambioDeVehiculo.java
+++ src/modelo/SorpresaCambioDeVehiculo.java
@@ -4,17 +4,21 @@ public class SorpresaCambioDeVehiculo extends Sorpresa {
 
     public void interactuarCon(VehiculoMoto vehiculo) {
         // lo convierte a auto.
-
+        Vehiculo nuevoVehiculo = VehiculoAuto.nuevoVehiculo(vehiculo);
+        vehiculo.cambiarA(nuevoVehiculo);
     }
 
     public void interactuarCon(Vehiculo4x4 vehiculo) {
         // a moto
+        Vehiculo nuevoVehiculo = VehiculoMoto.nuevoVehiculo(vehiculo);
+        vehiculo.cambiarA(nuevoVehiculo);
 
     }
 
     public void interactuarCon(VehiculoAuto vehiculo) {
         // auto a 4x4
-
+        Vehiculo nuevoVehiculo = Vehiculo4x4.nuevoVehiculo(vehiculo);
+        vehiculo.cambiarA(nuevoVehiculo);
     }
 
 }
