8f6ec66eb6643e25fdc81f395e0deead14b99436
cristian3629 <cristian3629@gmail.com>
2013-11-14 02:54:52 +0000

    actualizando todos los vehiculos

--- src/modelo/VehiculoMoto.java
+++ src/modelo/VehiculoMoto.java
@@ -3,11 +3,17 @@ package modelo;
 public class VehiculoMoto extends Vehiculo {
 
     public VehiculoMoto(Vector posicionInicial, int puntajeInicial) {
-    	super();
+        super();
         this.posicion = posicionInicial;
         this.cantidadDeMovimientos = puntajeInicial;
+        // esta porcion de codigo esta tanto vehiculo4x4 y vehiculoAuto
     }
 
-    protected void pasarPorCalle(Calle calle) {}
-    
+    protected void pasarPorCalle(Calle calle) {
+    }
+
+    public void aplicarEvento(Sorpresa sorpresa) {
+        sorpresa.interactuarCon(this);
+
+    }
 }
