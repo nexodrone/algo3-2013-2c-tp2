40e7fb4a5e8504e59880f00e31ab2ef6be6d482e
cristian3629 <cristian3629@gmail.com>
2013-11-14 16:46:14 +0000

    arreglando conflicto, agregue metodo cambiarA(nuevoVehiculo)

--- src/modelo/Juego.java
+++ src/modelo/Juego.java
@@ -5,43 +5,46 @@ import excepciones.MovimientoNoRealizadoException;
 
 public class Juego {
 
-	private Tablero tablero;
-	private Vehiculo vehiculo;
-	private Vector posicionGanadora;
+    private Tablero tablero;
+    private Vehiculo vehiculo;
+    private Vector posicionGanadora;
 
-	public Juego(Tablero tablero, Vehiculo vehiculo, Vector posicionGanadora) {
-		this.tablero = tablero;
-		this.vehiculo = vehiculo;
-		this.posicionGanadora = posicionGanadora;
-	}
-	
-	public void setPosicionGanadora(Vector posicionGanadora) {
-		this.posicionGanadora = posicionGanadora;
-	}
+    public Juego(Tablero tablero, Vehiculo vehiculo, Vector posicionGanadora) {
+        this.tablero = tablero;
+        this.vehiculo = vehiculo;
+        this.posicionGanadora = posicionGanadora;
+    }
 
-	public Vehiculo getVehiculo() {
-		return this.vehiculo;
-	}
+    public void setPosicionGanadora(Vector posicionGanadora) {
+        this.posicionGanadora = posicionGanadora;
+    }
 
-    public void realizarJugadaEnDireccion(Vector direccion)
-    		throws MovimientoInvalidoExcepcion
-    {
-    	Vector nuevaPosicion = vehiculo.calcularSiguientePosicion(direccion);
-    	Bocacalle bocacalleActual = tablero.getBocacalleEnPosicion(vehiculo.getPosicion());
-    	
-    	Calle calleATransitar = bocacalleActual.obtenerCalleEnDireccion(direccion);
-    	if (tablero.posicionValida(nuevaPosicion)) {
-    		try {
-    			vehiculo.moverEnDireccion(direccion, calleATransitar);
-    		}catch(MovimientoNoRealizadoException e) {
-    			System.out.print("Imposible mover en esa dirección.");
-    		}
-    	}
-    	else throw new MovimientoInvalidoExcepcion();
-    	
-    	// POR ULTIMO, VERIFICAMOS SI LA JUGADA HIZO QUE HAYA GANADO.
-    	if (posicionGanadora.equals(vehiculo.getPosicion())){
-    		// GANO. ACA TERMINA LA JUGADA.
-    	}   
+    public Vehiculo getVehiculo() {
+        return this.vehiculo;
+    }
+
+    public void realizarJugadaEnDireccion(Vector direccion) throws MovimientoInvalidoExcepcion {
+        Vector nuevaPosicion = vehiculo.calcularSiguientePosicion(direccion);
+        Bocacalle bocacalleActual = tablero.getBocacalleEnPosicion(vehiculo.getPosicion());
+
+        Calle calleATransitar = bocacalleActual.obtenerCalleEnDireccion(direccion);
+        if (tablero.posicionValida(nuevaPosicion)) {
+            try {
+                vehiculo.moverEnDireccion(direccion, calleATransitar);
+            } catch (MovimientoNoRealizadoException e) {
+                System.out.print("Imposible mover en esa direcciï¿½n.");
+            }
+        } else
+            throw new MovimientoInvalidoExcepcion();
+
+        // POR ULTIMO, VERIFICAMOS SI LA JUGADA HIZO QUE HAYA GANADO.
+        if (posicionGanadora.equals(vehiculo.getPosicion())) {
+            // GANO. ACA TERMINA LA JUGADA.
+        }
+    }
+
+    public void cambiarVehiculo(Vehiculo nuevoVehiculo) {
+        System.out.print("cambio de vehiculo");
+        vehiculo = nuevoVehiculo;
     }
 }
--- src/modelo/Vehiculo4x4.java
+++ src/modelo/Vehiculo4x4.java
@@ -3,13 +3,10 @@ package modelo;
 import excepciones.ImposiblePasarPorCalleException;
 import excepciones.PasajeBloqueadoPorPiqueteExcepcion;
 
-
 public class Vehiculo4x4 extends Vehiculo {
 
     public Vehiculo4x4(Vector posicionInicial, int puntajeInicial) {
-        super();
-        this.posicion = posicionInicial;
-        this.cantidadDeMovimientos = puntajeInicial;
+        super(posicionInicial, puntajeInicial);
         // esta porcion de codigo esta tanto en VehiculoMoto y VehiculoAuto
     }
 
@@ -22,16 +19,20 @@ public class Vehiculo4x4 extends Vehiculo {
         return nuevoVehiculo;
     }
 
-    public void pasarPorCalle(Calle calleAPasar) throws ImposiblePasarPorCalleException
-    {
-    	Obstaculo obstaculo = calleAPasar.getObstaculo();
-    	if( obstaculo != null )
-    		try {
-    			obstaculo.interactuarCon(this);
-    		}catch(PasajeBloqueadoPorPiqueteExcepcion e){
-    			System.out.print("Calle bloqueada por Castells.\n");
-    			throw new ImposiblePasarPorCalleException();
-    		}
+    public void pasarPorCalle(Calle calleAPasar) throws ImposiblePasarPorCalleException {
+        Obstaculo obstaculo = calleAPasar.getObstaculo();
+        if (obstaculo != null)
+            try {
+                obstaculo.interactuarCon(this);
+            } catch (PasajeBloqueadoPorPiqueteExcepcion e) {
+                System.out.print("Calle bloqueada por Castells.\n");
+                throw new ImposiblePasarPorCalleException();
+            }
+    }
+
+    public static Vehiculo nuevoVehiculo(Vehiculo vehiculo) {
+        Vehiculo nuevoVehiculo = new Vehiculo4x4(vehiculo.getPosicion(), vehiculo.getCantidadDeMovimientos());
+        System.out.println("devolvio una 4x4");
+        return nuevoVehiculo;
     }
 }
-
