590bd4c00a3dcb92b147989fd704d999c7e6507f
cristian3629@gmail.com <cristian3629@gmail.com@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-11-14 16:48:14 +0000

    arreglando conflicto, agregue metodo cambiarA(nuevoVehiculo)

--- test/modeloTests/SorpresaFavorableTest.java
+++ test/modeloTests/SorpresaFavorableTest.java
@@ -1,18 +1,20 @@
 package modeloTests;
 
 import static org.junit.Assert.assertEquals;
-import org.junit.Test;
 import modelo.SorpresaFavorable;
 import modelo.Vector;
 import modelo.VehiculoAuto;
 
+import org.junit.Test;
+
 public class SorpresaFavorableTest {
 
-	@Test
-	public void testDeberiaRestarCorrectamentePuntaje() {
-		VehiculoAuto auto = new VehiculoAuto(new Vector(0,0), 76);
-		SorpresaFavorable sf = new SorpresaFavorable();
-		sf.interactuarCon(auto);
-		assertEquals(auto.getCantidadDeMovimientos(),61);
-	}
+    @Test
+    public void testDeberiaRestarCorrectamentePuntaje() {
+        VehiculoAuto auto = new VehiculoAuto(new Vector(0, 0), 76);
+        SorpresaFavorable sf = new SorpresaFavorable();
+        sf.interactuarCon(auto);
+        assertEquals(auto.getCantidadDeMovimientos(), 61);
+    }
+    // Aca faltaria agregar que se comporte de la misma manera con los demas vehiculos
 }
