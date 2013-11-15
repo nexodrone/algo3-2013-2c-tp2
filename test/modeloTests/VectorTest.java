8c3933d4ecfabb1898313ecb7fffd39f0ddab198
nexodrone <nexodrone@gmail.com>
2013-11-15 17:10:38 +0000

    Elimine codigo muerto del Vector.

--- test/modeloTests/VectorTest.java
+++ test/modeloTests/VectorTest.java
@@ -28,38 +28,4 @@ public class VectorTest {
         assertFalse(vector1.equals(vector2));
     }
 
-    @Test
-    public void testAsignar() {
-        Vector vector1 = new Vector(1, 1);
-        Vector vector2 = new Vector(2, 3);
-        vector1.asignar(vector2);
-        assertTrue(vector1.x() == 2 && vector1.y() == 3);
-    }
-
-    @Test
-    public void testSuma() {
-        Vector vector1 = new Vector(1, 2);
-        Vector vector2 = new Vector(5, 10);
-        Vector suma = vector1.sumar(vector2);
-        Vector otroVector = new Vector(6, 12);
-        assertTrue(suma.equals(otroVector));
-    }
-
-    @Test
-    public void testPorEscalar0() {
-        Vector vector = new Vector(5, 7);
-        Vector nuevoVector = new Vector(0, 0);
-        Vector otroVector = vector.porEscalar(0);
-
-        assertTrue(otroVector.equals(nuevoVector));
-    }
-
-    @Test
-    public void testPorEscalar() {
-        Vector vector = new Vector(5, 7);
-        Vector nuevoVector = vector.porEscalar(3);
-        Vector otroVector = new Vector(15, 21);
-
-        assertTrue(otroVector.equals(nuevoVector));
-    }
 }
