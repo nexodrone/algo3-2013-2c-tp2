604de277bd9928a189b3e1796d752eec5d8a2b59
nexodrone@gmail.com <nexodrone@gmail.com@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-11-15 17:10:38 +0000

    Elimine codigo muerto del Vector.

--- src/modelo/Vector.java
+++ src/modelo/Vector.java
@@ -3,10 +3,6 @@ package modelo;
 public class Vector {
     private int x, y;
 
-    public Vector(Vector vector) {
-        this.asignar(vector);
-    }
-
     public Vector(int enX, int enY) {
         x = enX;
         y = enY;
@@ -20,29 +16,12 @@ public class Vector {
         return y;
     }
 
-    public void setX(int a) {
-        x = a;
-    }
-
-    public void setY(int a) {
-        y = a;
-    }
-
     public boolean equals(Vector vector) {
         boolean igualX = ((this.x()) == (vector.x()));
         boolean igualY = ((this.y()) == (vector.y()));
         return (igualX && igualY);
     }
 
-    public void asignar(Vector vector) {
-        x = vector.x();
-        y = vector.y();
-    }
-
-    public Vector copy() {
-        Vector nuevoVector = new Vector(x, y);
-        return nuevoVector;
-    }
 
     public void incrementarX(int incremento) {
         x = x + incremento;
@@ -50,14 +29,6 @@ public class Vector {
 
     public void incrementarY(int incremento) {
         y = y + incremento;
-    }
-
-    public Vector sumar(Vector vector) {
-        return new Vector(vector.x() + x, vector.y() + y);
-    }
-
-    public Vector porEscalar(int escalar) {
-        return new Vector(x * escalar, y * escalar);
     }
 
     public String asString() {
