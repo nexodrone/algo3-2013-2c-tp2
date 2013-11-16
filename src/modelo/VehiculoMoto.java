dd77aa9429dac46d976ab7ed7228d8f07aa7915c
cabrerajjorge <cabrerajjorge@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-11-16 18:53:37 +0000

    JorgeCabrera: les dejo la idea grafica del tests en .doc si les pasan el debugger hay algo raro creo que tiene que ver por como se unifican las calles

--- src/modelo/VehiculoMoto.java
+++ src/modelo/VehiculoMoto.java
@@ -15,8 +15,10 @@ public class VehiculoMoto extends Vehiculo {
         Obstaculo obstaculo = calleAPasar.getObstaculo();
         if (obstaculo != null) { obstaculo.interactuarCon(this); };
         Sorpresa sorpresa = calleAPasar.getSorpresa(); /* si llego hasta aca entonces no hay problema con obstaculo */
-        if (sorpresa != null) { sorpresa.interactuarCon(this);
-        						calleAPasar.setSorpresa(null); };
+        if (sorpresa != null) { 
+        	sorpresa.interactuarCon(this);
+        	calleAPasar.setSorpresa(null); 
+        };
         this.cantidadDeMovimientos++;
      }
 
