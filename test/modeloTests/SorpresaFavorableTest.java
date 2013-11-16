e190ee5aaf4b4d251a9571f56ef115fed4438a66
nexodrone@gmail.com <nexodrone@gmail.com@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-11-16 13:22:57 +0000

    Test faltantes para Vector, SorpresaFavorable y Desfavorable.

--- test/modeloTests/SorpresaFavorableTest.java
+++ test/modeloTests/SorpresaFavorableTest.java
@@ -3,19 +3,39 @@ package modeloTests;
 import static org.junit.Assert.assertEquals;
 import modelo.SorpresaFavorable;
 import modelo.Posicion;
+import modelo.Vehiculo4x4;
 import modelo.VehiculoAuto;
+import modelo.VehiculoMoto;
 
 import org.junit.Test;
 
 public class SorpresaFavorableTest {
 
     @Test
-    public void testDeberiaRestarCorrectamentePuntaje() {
+    public void testDeberiaRestarCorrectamentePuntajeAlAuto() {
         VehiculoAuto auto = new VehiculoAuto(new Posicion(0,0));
         auto.setCantidadDeMovimientos(76);
         SorpresaFavorable sorpresaFavorable = new SorpresaFavorable();
         sorpresaFavorable.interactuarCon(auto);
         assertEquals(auto.getCantidadDeMovimientos(), 61);
     }
-    // Aca faltaria agregar que se comporte de la misma manera con los demas vehiculos
+
+    @Test
+    public void testDeberiaRestarCorrectamentePuntajeAl4x4() {
+		Vehiculo4x4 todoterreno = new Vehiculo4x4(new Posicion(0,0));
+		todoterreno.setCantidadDeMovimientos(76);
+        SorpresaFavorable sorpresaFavorable = new SorpresaFavorable();
+        sorpresaFavorable.interactuarCon(todoterreno);
+        assertEquals(todoterreno.getCantidadDeMovimientos(), 61);
+    }
+    
+    @Test
+    public void testDeberiaRestarCorrectamentePuntajeALaMoto() {
+		VehiculoMoto moto = new VehiculoMoto(new Posicion(0,0));
+		moto.setCantidadDeMovimientos(76);
+        SorpresaFavorable sorpresaFavorable = new SorpresaFavorable();
+        sorpresaFavorable.interactuarCon(moto);
+        assertEquals(moto.getCantidadDeMovimientos(), 61);
+    }
+    
 }
