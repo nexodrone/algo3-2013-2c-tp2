3574f792f5505aa5f7f16057798f766f0a75b460
cristian3629 <cristian3629@gmail.com>
2013-11-20 01:34:48 +0000

    subiendo al clase Boton

---
+++ src/vista/Boton.java
@@ -0,0 +1,16 @@
+package vista;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class Boton extends JButton {
+    int tamX = 218;
+    int tamY = 54;
+
+    public Boton(int posX, int posY, String direccionIm, String nombre) {
+        super(nombre);
+        ImageIcon icono = new ImageIcon(direccionIm);
+        this.setIcon(icono);
+        this.setBounds(posX, posY, tamX, tamY);
+    }
+}
