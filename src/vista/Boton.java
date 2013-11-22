1e8600b1ec2aaf7973db3104ee0198db40b3a6dd
nexodrone <nexodrone@gmail.com>
2013-11-22 00:07:34 +0000

    Agrego el diagrama de hoy tal cual como lo tengo aca. Trae la idea general: hay metodos para eliminar, poner y cambiar de lugar. + Suposicion de que no puede haber + ke 1 obstaculo y sorpresa por calle. En la clase Boton me salia error pues faltaba un punto en this.

--- src/vista/Boton.java
+++ src/vista/Boton.java
@@ -10,7 +10,7 @@ public class Boton extends JButton {
     public Boton(int posX, int posY, String direccionIm, String nombre) {
         super(nombre);
         ImageIcon icono = new ImageIcon(direccionIm);
-        thissetIcon(icono);
+        this.setIcon(icono);
         this.setBounds(posX, posY, tamX, tamY);
     }
 }
