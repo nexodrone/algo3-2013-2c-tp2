a938c90ad3f1d26fd7b488f2beee858831d65a11
nexodrone <nexodrone@gmail.com>
2013-11-23 15:56:28 +0000

    Actualizando Nivel y sus tests en funcion de atributos publicos.

--- src/modelo/Nivel.java
+++ src/modelo/Nivel.java
@@ -9,31 +9,12 @@ public class Nivel {
 	public int cantidadDeObstaculos;
 	
 	public Nivel() {
+
 		this.movimientosDisponibles = 30;
 		this.filasTablero = 10;
 		this.columnasTablero = 10;
 		this.cantidadDeSorpresas = 5;
 		this.cantidadDeObstaculos = 5;
-	}
-	
-	public int getCantidadDeMovimientos(){
-		return movimientosDisponibles;
-	}
-	
-	public int getCantidadDeFilas(){
-		return filasTablero;
-	}
-	
-	public int getCantidadDeColumnas(){
-		return columnasTablero;
-	}
-	
-	public int getCantidadDeSorpresas(){
-		return cantidadDeSorpresas;
-	}
-	
-	public int getCantidadDeObstaculos(){
-		return cantidadDeObstaculos;
 	}
 	
 }
