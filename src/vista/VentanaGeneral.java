6e27cc7ece13c8d2f47b433acba4e7902d1303e5
cristian3629 <cristian3629@gmail.com>
2013-11-23 20:05:49 +0000

    agregando ventanas

---
+++ src/vista/VentanaGeneral.java
@@ -0,0 +1,30 @@
+package vista;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class VentanaGeneral extends JFrame {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    JFrame frame;
+
+    public VentanaGeneral(String titulo) {
+        super();
+        this.setSize(800, 481);
+        this.setResizable(false);
+        this.setTitle(titulo);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame = this;
+    }
+
+    public void addImagenDeFondo(String direccion) {
+        ImageIcon iconoNuevaPartida = new ImageIcon(direccion);
+        JLabel icono = new JLabel(iconoNuevaPartida);
+        icono.setBounds(0, 0, 800, 481);
+        this.getContentPane().add(icono);
+    }
+}
