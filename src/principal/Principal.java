1463d16993dbac25965537faf06f6e3f87642d77
nexodrone@gmail.com <nexodrone@gmail.com@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-11-25 17:34:50 +0000

    Se puede saltar entre las dos ventanas, se puede correr el Principal. Por ahor alo hice con mis ventanas y botones de java.

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,17 +1,14 @@
 package principal;
 
-import control.Controlador;
 import modelo.Juego;
+import vista.VentanaPrincipal;
+import control.Controlador;
 
 public class Principal {
 	
 	public static void main(String args[]) {
 		
 		Juego juego = new Juego();
-		
 		Controlador control = new Controlador(juego);
-		
-		
-		
 	}
 }
