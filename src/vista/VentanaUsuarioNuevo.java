04486a0ff6627fdc0879cb170c58d53fb7ef2cf4
cabrerajjorge <cabrerajjorge@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-11-25 21:31:57 +0000

    JorgeCabrera: agrego ventana comenzar partida

--- src/vista/VentanaUsuarioNuevo.java
+++ src/vista/VentanaUsuarioNuevo.java
@@ -5,7 +5,9 @@ import javax.swing.*;
 
 public class VentanaUsuarioNuevo extends Ventana {
 	
+	private JLabel etiqueta = new JLabel("Por favor introduzca su nombre");
 	private JButton botonVolver = new JButton("Volver");
+	private JButton botonGuardar = new JButton("Guardar");
 	private JTextField nombre = new JTextField();
 	
 	public VentanaUsuarioNuevo() {
@@ -14,10 +16,14 @@ public class VentanaUsuarioNuevo extends Ventana {
 		JPanel panelUsuarioNuevo = new JPanel();
 		panelUsuarioNuevo.setLayout(null);
 		
-		this.nombre.setBounds(200, 60, 200, 20);
-		this.botonVolver.setBounds(200,100,200,30);
+		etiqueta.setBounds(210, 50, 200, 30);
+		nombre.setBounds(200, 90, 200, 20);
+		botonGuardar.setBounds(70,130,200,30);
+		botonVolver.setBounds(320,130,200,30);
 		
+		panelUsuarioNuevo.add(etiqueta);
 		panelUsuarioNuevo.add(nombre);
+		panelUsuarioNuevo.add(botonGuardar);
 		panelUsuarioNuevo.add(botonVolver);
 		
 		this.add(panelUsuarioNuevo);
@@ -26,5 +32,8 @@ public class VentanaUsuarioNuevo extends Ventana {
 	public void agregarEscuchaVolver(ActionListener escuchaVolver) {
 		botonVolver.addActionListener(escuchaVolver);
 	}
-
+	
+	public void agregarEscuchaGuardar(ActionListener escuchaGuardar){
+		botonGuardar.addActionListener(escuchaGuardar);
+	}
 }
