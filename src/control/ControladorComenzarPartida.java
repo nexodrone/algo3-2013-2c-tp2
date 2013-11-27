cdfe0f326d47f977520c44d5d5a827ff7e7b7b7d
cabrerajjorge <cabrerajjorge@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-11-27 22:39:42 +0000

    JorgeCabrera: creado el método que crea una partida en el controladorComenzarPartida que usa la clase constructor de partida. Este método por favor veánlo

--- src/control/ControladorComenzarPartida.java
+++ src/control/ControladorComenzarPartida.java
@@ -3,11 +3,16 @@ package control;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import modelo.ConstructorDePartida;
 import modelo.Juego;
 import modelo.Jugador;
+import modelo.Nivel;
 import modelo.Partida;
 import modelo.Posicion;
 import modelo.Tablero;
+import modelo.Vehiculo;
+import modelo.Vehiculo4x4;
+import modelo.VehiculoAuto;
 import modelo.VehiculoMoto;
 import vista.PanelComenzarPartida;
 import vista.Ventana;
@@ -44,19 +49,37 @@ public class ControladorComenzarPartida extends Controlador{
 	public class EscuchaComenzarPartida implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e){
+			juego.setJugador(new Jugador(nombre));
 			String nivelSeleccionado = panelComenzarPartida.obtenerNivelSeleccionado();
 			String vehiculoSeleccionado = panelComenzarPartida.obtenerVehiculoSeleccionado();
-			juego.setJugador(new Jugador(nombre));
-			Tablero tablero = new Tablero(20,5);
-			VehiculoMoto moto = new VehiculoMoto(new Posicion(0,0));
-			moto.setJuegoActual(juego);
-			Partida partida = new Partida(tablero, moto, new Posicion (6,6), 50);
+			Nivel nivel = obtenerNivelSeleccionado(nivelSeleccionado);
+			Partida partida = construirPartidaSeleccionada(vehiculoSeleccionado,nivel);
+			partida.getVehiculo().setJuegoActual(juego);
 			juego.setPartida(partida);			
 		
 			ventana.remove(panelComenzarPartida);
 			ControladorPartida controlador = new ControladorPartida(juego,ventana,nivelSeleccionado,vehiculoSeleccionado);
 		}
 	}
+
 	
+	public Nivel obtenerNivelSeleccionado(String nivelSeleccionado){
+		//deberia crear el nivel según los parametros del .xml
+		return new Nivel();
+	}
 	
+	public Partida construirPartidaSeleccionada(String vehiculoSeleccionado,Nivel nivel){
+		if( vehiculoSeleccionado == "Moto"){
+			ConstructorDePartida constructorMoto = new ConstructorDePartida();
+			return constructorMoto.construirPartidaConMoto(nivel);
+		}else if (vehiculoSeleccionado == "Auto"){
+			ConstructorDePartida constructorAuto = new ConstructorDePartida();
+			return constructorAuto.construirPartidaConAuto(nivel);
+		}else{
+			ConstructorDePartida constructor4x4 = new ConstructorDePartida();
+			return constructor4x4.construirPartidaCon4x4(nivel);
+		}	
+	}
+	
+
 }
