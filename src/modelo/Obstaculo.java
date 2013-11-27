a5f955b98f5693bcb00b7c626cf2f4ff9e5b2e42
cristian3629@gmail.com <cristian3629@gmail.com@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-11-27 17:18:28 +0000

    se agrega la interfaz Evento. herede Obstaculo y Sorpresa, y Piquete no testea ninguna excepcion

--- src/modelo/Obstaculo.java
+++ src/modelo/Obstaculo.java
@@ -1,13 +1,9 @@
 package modelo;
 
-import modelo.excepciones.PasajeBloqueadoPorPiqueteExcepcion;
 
-public abstract class Obstaculo {
+public abstract class Obstaculo implements Evento {
 
-	public Obstaculo() {}
-	
-	public abstract void interactuarCon(VehiculoAuto auto) throws PasajeBloqueadoPorPiqueteExcepcion;
-	public abstract void interactuarCon(Vehiculo4x4 todoterreno) throws PasajeBloqueadoPorPiqueteExcepcion;
-	public abstract void interactuarCon(VehiculoMoto moto);
+    public Obstaculo() {
+    }
 
 }
