fb3761882bb0d27076e7cf8e3085897985d5c8c2
m.iglesias91@gmail.com <m.iglesias91@gmail.com@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-11-27 20:36:40 +0000

    Agregado de test a Partida

--- test/modeloTests/PartidaTest.java
+++ test/modeloTests/PartidaTest.java
@@ -1,7 +1,18 @@
 package modeloTests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import modelo.Direccion;
+import modelo.ObstaculoPiquete;
+import modelo.ObstaculoPozo;
 import modelo.Partida;
+import modelo.Posicion;
+import modelo.SorpresaDesfavorable;
+import modelo.SorpresaFavorable;
+import modelo.Tablero;
+import modelo.Vehiculo;
+import modelo.VehiculoAuto;
+
 import org.junit.Test;
 
 public class PartidaTest {
@@ -11,5 +22,35 @@ public class PartidaTest {
 		Partida unaPartida = new Partida();
 		assertNotNull(unaPartida);
 	}
+	
+	@Test
+	public void testDeberiaGuardarYCargarPartida() throws Exception {
+        Tablero tablero = new Tablero(4, 6);
+        Vehiculo vehiculo = new VehiculoAuto(new Posicion(0, 0));
+        vehiculo.setCantidadDeMovimientos(0);
+        Posicion posicionGanadora = new Posicion(3, 4);
+        int movimientosDisponibles = 100;
+
+        Partida unaPartida = new Partida(tablero, vehiculo, posicionGanadora, movimientosDisponibles);
+        
+        Direccion norte = new Direccion(0, 1);
+        Direccion sur = new Direccion(0, -1);
+        Direccion este = new Direccion(1, 0);
+        Direccion oeste = new Direccion(-1, 0);
+        
+        tablero.getBocacalleEnPosicion(new Posicion(1, 0)).getCalleEnDireccion(norte).setObstaculo(new ObstaculoPiquete());
+        tablero.getBocacalleEnPosicion(new Posicion(2, 3)).getCalleEnDireccion(sur).setObstaculo(new ObstaculoPozo());
+        tablero.getBocacalleEnPosicion(new Posicion(1, 4)).getCalleEnDireccion(este).setSorpresa(new SorpresaDesfavorable());
+        tablero.getBocacalleEnPosicion(new Posicion(3, 4)).getCalleEnDireccion(oeste).setSorpresa(new SorpresaFavorable());
+        
+        unaPartida.guardar("partidaTest.xml");
+        
+        Partida otraPartida = Partida.recuperar("partidaTest.xml");
+        
+        assertEquals(otraPartida.getVehiculo().getCantidadDeMovimientos(), vehiculo.getCantidadDeMovimientos());
+        assertEquals(otraPartida.getPosicionGanadora().asString(), posicionGanadora.asString());
+	}
+	
+
     
 }
