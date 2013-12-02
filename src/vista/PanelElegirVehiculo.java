5245f0179bd4ef3c27d18478038cf09b4b26ce7b
cabrerajjorge <cabrerajjorge@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-12-02 21:26:16 +0000

    JorgeCabrera: borre los controladores y paneles que ya no se usaban y agrego una ventana que le indica al usuario que por favor ingrese algun nivel o vehiculo si le da aceptar sin seleccionar nada

---
+++ src/vista/PanelElegirVehiculo.java
@@ -0,0 +1,105 @@
+package vista;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+public class PanelElegirVehiculo extends JPanel {
+	
+	private JButton botonAceptar = new JButton("Aceptar");
+	private JButton botonVolver = new JButton("Volver");
+	private JLabel etiqueta = new JLabel("Por favor seleccione vehiculo");
+	private JRadioButton botonAuto = new JRadioButton("Auto");
+	private JRadioButton botonMoto = new JRadioButton("Moto");
+	private JRadioButton boton4x4 = new JRadioButton("4x4");
+	private ButtonGroup bgroup = new ButtonGroup();
+	private JLabel vehiculoAuto;
+	private JLabel vehiculoMoto;
+	private JLabel vehiculo4x4;
+	private String nivel;
+	
+
+
+	public PanelElegirVehiculo(String nivelSeleccionado) {
+		this.setLayout(null);
+		this.nivel= nivelSeleccionado;
+		this.etiqueta.setBounds(530, 60, 220, 10);
+		this.vehiculoAuto=obtenerImagenDeVehiculo(200,100,"src/vista/imagenes/vehiculoAuto.jpg");
+		this.vehiculoMoto=obtenerImagenDeVehiculo(490,100,"src/vista/imagenes/vehiculoMoto.jpg");
+		this.vehiculo4x4=obtenerImagenDeVehiculo(780,100,"src/vista/imagenes/vehiculo4x4.jpg");
+	
+		this.botonAuto.setBounds(300,370,80,30);
+		this.botonMoto.setBounds(590,370, 100, 30);
+		this.boton4x4.setBounds(860,370,80,30);
+		
+		this.botonAceptar.setBounds(500,430,200,30);
+		this.botonVolver.setBounds(500,480,200, 30);
+		this.bgroup.add(botonAuto);
+		this.bgroup.add(botonMoto);
+		this.bgroup.add(boton4x4);
+		
+		this.add(etiqueta);
+		this.add(vehiculoAuto);
+		this.add(vehiculoMoto);
+		this.add(vehiculo4x4);
+		this.add(botonAuto);
+		this.add(botonMoto);
+		this.add(boton4x4);
+		this.add(botonAceptar);
+		this.add(botonVolver);
+	}
+	
+	public void agregarEscuchaAceptar(ActionListener escuchaAceptar){
+		this.botonAceptar.addActionListener(escuchaAceptar);
+	}
+		
+	public void agregarEscuchaVolver(ActionListener escuchaVolver){
+		this.botonVolver.addActionListener(escuchaVolver);
+	}
+	
+	public void mostrarMensajeError() {
+		JOptionPane.showMessageDialog(this, "Error! Se cargara nivel por defecto.", "Error", JOptionPane.ERROR_MESSAGE);
+	}
+	
+	public JLabel obtenerImagenDeVehiculo(int posX,int posY,String direccion){
+		JLabel imagen = new JLabel("");
+		ImageIcon icono = new ImageIcon(direccion);
+		imagen.setIcon(icono);
+		imagen.setBounds(posX, posY, 250, 250);
+		return imagen;
+	}
+
+	public String obtenerVehiculoSeleccionado(){
+		if (botonAuto.isSelected()){
+			return "Auto";
+		}else if (boton4x4.isSelected()){
+			return "4x4";
+		}else{
+			return "Moto";
+		}
+	}
+	
+	public boolean ningunCampoSeleccionado(){
+		if (botonAuto.isSelected() == false && 
+			boton4x4.isSelected() == false && 
+			botonMoto.isSelected()==false){
+			return true;
+		} else return false;
+	}
+	
+	public void mostrarMensajeCampoVacio() {
+		JOptionPane.showMessageDialog(this, "Por favor seleccione un vehiculo.", "Vehiculo no seleccionado", JOptionPane.WARNING_MESSAGE);
+	}
+	
+	public String obtenerNivelSeleccionado(){
+		return nivel;
+	}
+	
+}
+
