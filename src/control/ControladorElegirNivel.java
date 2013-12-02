cdf7ea9e56020ba96974929df2e48e1b33507276
cabrerajjorge <cabrerajjorge@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-12-02 21:01:24 +0000

    JorgeCabrera: agrego venana para escojer el vehiculo con su controlador.

---
+++ src/control/ControladorElegirNivel.java
@@ -0,0 +1,42 @@
+package control;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import vista.PanelElegirNivel;
+
+public class ControladorElegirNivel extends Controlador{
+	
+	private PanelElegirNivel panelElegirNivel;
+	
+	public ControladorElegirNivel(){
+		 this.agregarPanelLocal();
+	     ventana.setVisible(true);
+	}
+		
+	private void agregarPanelLocal() {
+		this.panelElegirNivel = new PanelElegirNivel();
+		this.panelElegirNivel.agregarEscuchaVolver(new EscuchaVolver());
+		this.panelElegirNivel.agregarEscuchaAceptar(new EscuchaComenzarPartida());
+		ventana.add(panelElegirNivel);
+	}
+
+	
+	public class EscuchaVolver implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            ventana.remove(panelElegirNivel);
+            ControladorMenuPrincipal contolador = new ControladorMenuPrincipal();
+        }
+    }
+	
+	public class EscuchaComenzarPartida implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e){
+			String nivelSeleccionado = panelElegirNivel.obtenerNivelSeleccionado();
+			ventana.remove(panelElegirNivel);
+			ControladorElegirVehiculo controlador = new ControladorElegirVehiculo(nivelSeleccionado);
+		}
+	}
+
+}
---
+++ src/control/ControladorElegirVehiculo.java
@@ -0,0 +1,66 @@
+package control;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import modelo.ConstructorDePartida;
+import modelo.Nivel;
+import modelo.Partida;
+import vista.PanelElegirVehiculo;
+
+
+public class ControladorElegirVehiculo extends Controlador{
+	private PanelElegirVehiculo panelElegirVehiculo;
+	
+	public ControladorElegirVehiculo(String nivelSeleccionado){
+		 this.agregarPanelLocal(nivelSeleccionado);
+	     ventana.setVisible(true);
+	}
+	
+	private void agregarPanelLocal(String nivelSeleccionado) {
+		this.panelElegirVehiculo = new PanelElegirVehiculo(nivelSeleccionado);
+		this.panelElegirVehiculo.agregarEscuchaVolver(new EscuchaVolver());
+		this.panelElegirVehiculo.agregarEscuchaAceptar(new EscuchaComenzarPartida());
+		ventana.add(panelElegirVehiculo);
+	}
+
+	
+	public class EscuchaVolver implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            ventana.remove(panelElegirVehiculo);
+            ControladorElegirNivel contolador = new ControladorElegirNivel();
+        }
+    }
+	
+
+	  public class EscuchaComenzarPartida implements ActionListener {
+	        @Override
+	        public void actionPerformed(ActionEvent e) {
+	            String nivelSeleccionado = panelElegirVehiculo.obtenerNivelSeleccionado();
+	            String vehiculoSeleccionado = panelElegirVehiculo.obtenerVehiculoSeleccionado();
+	            Nivel nivel = new Nivel();
+	            try {
+	                nivel = Nivel.cargarNivel("src/niveles/Nivel" + nivelSeleccionado + ".xml");
+	            } catch (Exception e1) {
+	            	panelElegirVehiculo.mostrarMensajeError();
+	            };
+	            Partida partida = construirPartidaSeleccionada(vehiculoSeleccionado, nivel);
+	            juego.setPartida(partida);
+	            ventana.remove(panelElegirVehiculo);
+	            ControladorPartida controlador = new ControladorPartida(ventana, nivelSeleccionado, vehiculoSeleccionado);
+	        }
+	    }
+
+	    public Partida construirPartidaSeleccionada(String vehiculoSeleccionado, Nivel nivel) {
+	        switch (vehiculoSeleccionado) {
+	            case "Moto":
+	                return ConstructorDePartida.construirPartidaConMoto(nivel);
+	            case "Auto":
+	                return ConstructorDePartida.construirPartidaConAuto(nivel);
+	            default:
+	                return ConstructorDePartida.construirPartidaCon4x4(nivel);
+	        }
+	    }
+
+}
