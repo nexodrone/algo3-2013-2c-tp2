8110a44e358ecc65dae5e6f634a7de69ccf72dd0
cristian3629@gmail.com <cristian3629@gmail.com@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-12-02 04:44:06 +0000

    Juego y Observador estan con el patron de Singleton, cambio de sintaxis

--- src/modelo/Principal.java
+++ src/modelo/Principal.java
@@ -4,11 +4,11 @@ import vista.Ventana;
 import control.ControladorBienvenido;
 
 public class Principal {
-	
-	public static void main(String args[]) {
-		
-		Juego juego = new Juego();
-		Ventana ventana = new Ventana();
-		ControladorBienvenido control = new ControladorBienvenido(juego, ventana);
-	}
+
+    public static void main(String args[]) {
+
+        Juego juego = Juego.getInstance();
+        Ventana ventana = new Ventana();
+        ControladorBienvenido control = new ControladorBienvenido(juego, ventana);
+    }
 }
