da347d5de38c2e9c7056f2516b19abaa8bdb2d2a
cristian3629 <cristian3629@gmail.com>
2013-12-04 16:28:19 +0000

    Ahora el tablero se dibuja solo una vez, la clase Board se llama ahora PanelZonaDeJuego

--- src/vista/Auto2.java
+++ src/vista/Auto2.java
@@ -4,18 +4,16 @@ import javax.swing.JFrame;
 
 public class Auto2 extends JFrame {
 
-	Board board;
-	int largoDeVentana = 500;
-	int anchoDeVentana = 500;
-	
-	public Auto2() {
+    PanelZonaDeJuego board;
+    int largoDeVentana = 500;
+    int anchoDeVentana = 500;
 
-    	board= new Board(largoDeVentana,anchoDeVentana);
-  
-    	this.setBounds(0, 0,anchoDeVentana,largoDeVentana);
-    	this.add(board);
-       
+    public Auto2() {
 
+        board = new PanelZonaDeJuego(largoDeVentana, anchoDeVentana);
+
+        this.setBounds(0, 0, anchoDeVentana, largoDeVentana);
+        this.add(board);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(anchoDeVentana, largoDeVentana);
         setLocationRelativeTo(null);
