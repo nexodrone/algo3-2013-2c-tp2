d45e89149dc79516581f2367ac218acd49be354e
cabrerajjorge <cabrerajjorge@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-12-04 23:43:03 +0000

    JorgeCabrera: cambio los xml adaptandose a nuestra zona de juego

--- src/niveles/EditorDeNiveles.java
+++ src/niveles/EditorDeNiveles.java
@@ -8,8 +8,8 @@ public class EditorDeNiveles {
 	public static void main(String args[]) throws Exception {
 		
 		Nivel nivel = new Nivel();
-		nivel.tablero = new Tablero(30,30);
-		nivel.movimientosDisponibles = 40;
+		nivel.tablero = new Tablero(21,14);
+		nivel.movimientosDisponibles = 25;
 		nivel.dificultad = 3;
 		
 		nivel.guardarNivel("src/niveles/NivelDificil.xml");
