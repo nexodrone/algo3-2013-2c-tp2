64c2a56b98443425b2f7f87b2a5ef6d56470274f
cristian3629 <cristian3629@gmail.com>
2013-12-04 15:07:09 +0000

    Agregando funcionalidad, para que el vehiculo gire

--- src/control/MyKeyListener.java
+++ src/control/MyKeyListener.java
@@ -18,15 +18,19 @@ public class MyKeyListener implements KeyListener {
     public void keyPressed(KeyEvent e) {
         System.out.println("keyPressed=" + KeyEvent.getKeyText(e.getKeyCode()));
         if (KeyEvent.getKeyText(e.getKeyCode()) == "Derecha") {
+            panel.girarHacia("Derecha");
             panel.nuevaPosicion(1, 0);
         }
         if (KeyEvent.getKeyText(e.getKeyCode()) == "Izquierda") {
+            panel.girarHacia("Izquierda");
             panel.nuevaPosicion(-1, 0);
         }
         if (KeyEvent.getKeyText(e.getKeyCode()) == "Arriba") {
+            panel.girarHacia("Arriba");
             panel.nuevaPosicion(0, -1);
         }
         if (KeyEvent.getKeyText(e.getKeyCode()) == "Abajo") {
+            panel.girarHacia("Abajo");
             panel.nuevaPosicion(0, 1);
         }
 
