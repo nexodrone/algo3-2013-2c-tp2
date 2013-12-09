4311fde5dc7592652c207c0d3c630ddac0eaa78d
nexodrone@gmail.com <nexodrone@gmail.com@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-12-09 18:22:20 +0000

    Botones!

--- src/vista/Boton.java
+++ src/vista/Boton.java
@@ -1,6 +1,7 @@
 package vista;
 
 import java.awt.Dimension;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class Boton extends JButton {
@@ -9,6 +10,20 @@ public class Boton extends JButton {
 
     public Boton(String texto) {
         super(texto);
+        
+        ImageIcon icono = new ImageIcon("src/vista/imagenes/btn.png");
+        this.setIcon(icono);
+        
+        ImageIcon iconoRoll = new ImageIcon("src/vista/imagenes/btn_roll.png");
+        this.setRolloverIcon(iconoRoll);
+        
+        ImageIcon iconoPress = new ImageIcon("src/vista/imagenes/btn_press.png");
+        this.setPressedIcon(iconoPress);
+        
+        this.setBorderPainted(false);
+        
+        this.setHorizontalTextPosition(JButton.CENTER);
+        this.setVerticalTextPosition(JButton.CENTER);
         this.setMaximumSize(dimension);
         this.setMinimumSize(dimension);
     }
