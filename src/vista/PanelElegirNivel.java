5f38294c1bb41c9d6528e1537661029d652eda98
nexodrone@gmail.com <nexodrone@gmail.com@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-12-09 15:41:34 +0000

    Termine con PanelEligirNivel. Obviamente se agrego complejidad, pero lo hice intuitivo.

--- src/vista/PanelElegirNivel.java
+++ src/vista/PanelElegirNivel.java
@@ -2,15 +2,13 @@ package vista;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyListener;
-
-
-
-
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -18,50 +16,28 @@ import javax.swing.JRadioButton;
 
 public class PanelElegirNivel extends JPanel {
 
-	private JButton botonAceptar = new JButton("Aceptar");
-	private JButton botonVolver = new JButton("Volver");
+	private Boton botonAceptar = new Boton("Aceptar");
+	private Boton botonVolver = new Boton("Volver");
 	private JLabel etiqueta = new JLabel("Por favor seleccione dificultad");
-	private JRadioButton botonFacil = new JRadioButton("Facil");
-	private JRadioButton botonIntermedio = new JRadioButton("Intermedio");
-	private JRadioButton botonDificil = new JRadioButton("Dificil");
-	private ButtonGroup bgroup = new ButtonGroup();
-	private JLabel tableroFacil;
-	private JLabel tableroIntermedio;
-	private JLabel tableroDificil;
+	private PanelImgBtn panelConImagenesYBotones = new PanelImgBtn();
 
 	public PanelElegirNivel() {
-		this.setLayout(null);
-		
+		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		this.setPreferredSize(new Dimension(1000,600));
-		this.setBackground(new Color(255,255,255,150));
+		this.setBackground(new Color(255,255,255,200));
 		
-		this.etiqueta.setBounds(470, 60, 220, 10);
-		this.tableroFacil=obtenerImagenTablero(100,100,"src/vista/imagenes/nivelFacil.png");
-		this.tableroIntermedio=obtenerImagenTablero(390,100,"src/vista/imagenes/nivelIntermedio.png");
-		this.tableroDificil=obtenerImagenTablero(680,100,"src/vista/imagenes/nivelDificil.png");
-	
-		this.botonFacil.setBounds(200,370,80,30);
-		this.botonIntermedio.setBounds(490,370, 100, 30);
-		this.botonDificil.setBounds(760,370,80,30);
-		
-		this.botonFacil.setOpaque(false);
-		this.botonIntermedio.setOpaque(false);
-		this.botonDificil.setOpaque(false);
-		
-		this.botonAceptar.setBounds(400,430,200,30);
-		this.botonVolver.setBounds(400,480,200, 30);
-		this.bgroup.add(botonFacil);
-		this.bgroup.add(botonIntermedio);
-		this.bgroup.add(botonDificil);
-		
+		this.etiqueta.setAlignmentX(CENTER_ALIGNMENT);
+		this.panelConImagenesYBotones.setAlignmentX(CENTER_ALIGNMENT);
+		this.botonAceptar.setAlignmentX(CENTER_ALIGNMENT);
+		this.botonVolver.setAlignmentX(CENTER_ALIGNMENT);
+
+		this.add(Box.createVerticalStrut(15));
 		this.add(etiqueta);
-		this.add(tableroFacil);
-		this.add(tableroIntermedio);
-		this.add(tableroDificil);
-		this.add(botonFacil);
-		this.add(botonIntermedio);
-		this.add(botonDificil);
+		this.add(Box.createVerticalStrut(20));
+		this.add(panelConImagenesYBotones);
+		this.add(Box.createVerticalStrut(20));
 		this.add(botonAceptar);
+		this.add(Box.createVerticalStrut(20));
 		this.add(botonVolver);
 	}
 	
@@ -72,41 +48,125 @@ public class PanelElegirNivel extends JPanel {
 	public void agregarEscuchaVolver(ActionListener escuchaVolver){
 		this.botonVolver.addActionListener(escuchaVolver);
 	}
-		
-	public JLabel obtenerImagenTablero(int posX,int posY,String direccion){
-		JLabel imagen = new JLabel("");
-		ImageIcon icono = new ImageIcon(direccion);
-		imagen.setIcon(icono);
-		imagen.setBounds(posX, posY, 250, 250);
-		return imagen;
-	}
 
-	public String obtenerNivelSeleccionado(){
-		if (botonFacil.isSelected()){
+	public String obtenerNivelSeleccionado() {
+		if (this.panelConImagenesYBotones.botonFacil.isSelected()){
 			return "Facil";
-		}else if(botonDificil.isSelected()){
+		}else if(this.panelConImagenesYBotones.botonDificil.isSelected()){
 			return "Dificil";
 		}else{
 			return "Intermedio";
 		}
 	}
-	
-	public boolean ningunCampoSeleccionado(){
-		if (botonFacil.isSelected() == false && 
-			botonDificil.isSelected() == false && 
-			botonIntermedio.isSelected()==false){
-			return true;
-		} else return false;
+
+	public boolean ningunCampoSeleccionado() {
+		return (panelConImagenesYBotones.bgroup.getSelection() == null);
 	}
-	
-	public void mostrarMensajeCampoVacio() {
+
+	public void mostrarMensajeNadaSeleccionado() {
 		JOptionPane.showMessageDialog(this, "Por favor seleccione un nivel.", "Nivel no seleccionado", JOptionPane.WARNING_MESSAGE);
 	}
 	
-	public void agregarEscuchaEnter(KeyListener escuchaEnter) {
-		this.botonFacil.addKeyListener(escuchaEnter);
-		this.botonIntermedio.addKeyListener(escuchaEnter);
-		this.botonDificil.addKeyListener(escuchaEnter);
+	private class PanelImgBtn extends JPanel {
+		
+		public JPanel panelFacil = new JPanel();
+		public JPanel panelIntermedio = new JPanel();
+		public JPanel panelDificil = new JPanel();
+		public JLabel tableroFacil = new JLabel();
+		public JLabel tableroIntermedio = new JLabel();
+		public JLabel tableroDificil = new JLabel();
+		private ButtonGroup bgroup = new ButtonGroup();
+		public JRadioButton botonFacil = new JRadioButton("Facil");
+		public JRadioButton botonIntermedio = new JRadioButton("Intermedio");
+		public JRadioButton botonDificil = new JRadioButton("Dificil");
+		
+		public PanelImgBtn() {
+			
+			this.setLayout(new FlowLayout());
+			this.setAlignmentX(CENTER_ALIGNMENT);
+			this.setBackground(new Color(0,0,0,0));
+			this.setMaximumSize(new Dimension(900,400));
+
+			this.agregarPanelConNivelFacil();
+			this.add(Box.createHorizontalStrut(20));
+			this.agregarPanelConNivelIntermedio();
+			this.add(Box.createHorizontalStrut(20));
+			this.agregarPanelConNivelDificil();
+		}
+
+		private void agregarPanelConNivelFacil() {
+			prepararPanelConNivel(panelFacil);
+			prepararImagenIlustrativa(tableroFacil, "src/vista/imagenes/nivelFacil.png");
+			prepararBoton(botonFacil);
+			
+			JLabel textoDescriptivo1 = new JLabel("Tablero chico.");
+			JLabel textoDescriptivo2 = new JLabel("Abundantes movimientos.");
+			textoDescriptivo1.setAlignmentX(CENTER_ALIGNMENT);
+			textoDescriptivo2.setAlignmentX(CENTER_ALIGNMENT);
+			
+			agregarComponentes(panelFacil, tableroFacil, textoDescriptivo1, textoDescriptivo2, botonFacil);
+
+			this.add(panelFacil);
+		}
+		
+		private void agregarPanelConNivelIntermedio() {
+			prepararPanelConNivel(panelIntermedio);
+			prepararImagenIlustrativa(tableroIntermedio, "src/vista/imagenes/nivelIntermedio.png");
+			prepararBoton(botonIntermedio);
+			
+			JLabel textoDescriptivo1 = new JLabel("Tablero intermedio.");
+			JLabel textoDescriptivo2 = new JLabel("Sufucientes movimientos.");
+			textoDescriptivo1.setAlignmentX(CENTER_ALIGNMENT);
+			textoDescriptivo2.setAlignmentX(CENTER_ALIGNMENT);
+			
+			agregarComponentes(panelIntermedio, tableroIntermedio, textoDescriptivo1, textoDescriptivo2, botonIntermedio);
+			
+			this.add(panelIntermedio);
+		}
+		
+		private void agregarPanelConNivelDificil() {
+			prepararPanelConNivel(panelDificil);
+			prepararImagenIlustrativa(tableroDificil, "src/vista/imagenes/nivelDificil.png");
+			prepararBoton(botonDificil);
+			
+			JLabel textoDescriptivo1 = new JLabel("Tablero grande.");
+			JLabel textoDescriptivo2 = new JLabel("Escasos movimientos.");
+			textoDescriptivo1.setAlignmentX(CENTER_ALIGNMENT);
+			textoDescriptivo2.setAlignmentX(CENTER_ALIGNMENT);
+			
+			agregarComponentes(panelDificil, tableroDificil, textoDescriptivo1, textoDescriptivo2, botonDificil);
+			
+			this.add(panelDificil);
+		}
+		
+		private void prepararPanelConNivel(JPanel panel) {
+			panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+			panel.setPreferredSize(new Dimension(250,400));
+			panel.setBackground(Color.WHITE);
+		}
+		
+		private void prepararImagenIlustrativa(JLabel label, String ruta) {
+			ImageIcon icono = new ImageIcon(ruta);
+			label.setIcon(icono);
+			label.setPreferredSize(new Dimension(250,250));
+			label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+			label.setAlignmentX(CENTER_ALIGNMENT);
+		}
+		
+		private void prepararBoton(JRadioButton boton) {
+			boton.setAlignmentX(CENTER_ALIGNMENT);
+			boton.setBackground(Color.WHITE);
+			this.bgroup.add(boton);
+		}
+		
+		private void agregarComponentes(JPanel panel, JLabel imagen, JLabel label1, JLabel label2, JRadioButton boton) {
+			panel.add(imagen);
+			panel.add(Box.createVerticalStrut(20));
+			panel.add(label1);
+			panel.add(label2);
+			panel.add(Box.createVerticalStrut(20));
+			panel.add(boton);
+		}
 	}
-	
+
 }
