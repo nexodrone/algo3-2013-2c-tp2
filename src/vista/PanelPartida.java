ee667ad3a83bbe0506efeb97933002e017c1e77c
nexodrone <nexodrone@gmail.com>
2013-12-09 20:55:39 +0000

    Mejorando un poco la vista de PanelPartida. Arreglan los bordes por favor. Les dejo los tomaños de la zona para que sale perfetco. Pero en algunos lugares ustedes tendran que sacar cosas tipo "+ longitudDeManzana".

--- src/vista/PanelPartida.java
+++ src/vista/PanelPartida.java
@@ -2,13 +2,15 @@ package vista;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.ActionMap;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.InputMap;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -20,41 +22,53 @@ import modelo.Tablero;
 
 public class PanelPartida extends JPanel {
 
-    private JButton botonGuardar = new JButton("Guardar");
-    private JButton botonVolver = new JButton("Volver");
-    private JLabel nota = new JLabel("Nota: al cerrar el programa o volver, todo progreso no guardado sera perdido.");
+    private Boton botonGuardar = new Boton("Guardar");
+    private Boton botonVolver = new Boton("Volver");
     private JLabel nombreUsuario;
     private JLabel dificultad;
     private JLabel vehiculoActual;
     private PanelZonaDeJuego panelZonaDelJuego;
-    private int anchoZonaDelJuego = 870;
-    private int largoZonaDelJuego = 610;
+    private JPanel panelInfo = new JPanel();
+    private int anchoZonaDelJuego = 870; //ideal = 860
+    private int largoZonaDelJuego = 610; //ideal = 580
     String nivelSeleccionado;
 
     public PanelPartida(String nombre, String dificultad, String vehiculo, Tablero tablero, Posicion posicionVehiculo, Posicion posicionLlegada) {
-        this.setLayout(null);
-        this.nivelSeleccionado = dificultad;
-        this.panelZonaDelJuego = new PanelZonaDeJuego(tablero, vehiculo, posicionVehiculo,posicionLlegada);
-
+        this.setLayout(new FlowLayout());
         this.setPreferredSize(new Dimension(1180, 680));
-        this.setBackground(new Color(255, 255, 255, 150));
+        this.setBackground(new Color(255, 255, 255, 200));
+        this.nivelSeleccionado = dificultad;
+        
+        this.panelInfo.setPreferredSize(new Dimension(250,610));
+        this.panelInfo.setLayout(new BoxLayout(panelInfo, BoxLayout.PAGE_AXIS));
+        this.panelInfo.setBackground(new Color(0,0,0,0));
+        this.nombreUsuario = new JLabel("Jugador:   " + nombre);
+        this.nombreUsuario.setAlignmentX(CENTER_ALIGNMENT);
+        this.dificultad = new JLabel("Dificultad:   " + dificultad);
+        this.dificultad.setAlignmentX(CENTER_ALIGNMENT);
+        this.vehiculoActual = new JLabel("Vehiculo:   " + vehiculo);
+        this.vehiculoActual.setAlignmentX(CENTER_ALIGNMENT);
+        
+        this.panelZonaDelJuego = new PanelZonaDeJuego(tablero, vehiculo, posicionVehiculo, posicionLlegada);
+        this.panelZonaDelJuego.setPreferredSize(new Dimension(anchoZonaDelJuego,largoZonaDelJuego));
 
-        this.nombreUsuario = new JLabel("Jugador: " + nombre);
-        this.dificultad = new JLabel("Dificultad: " + dificultad);
-        this.vehiculoActual = new JLabel("Vehiculo: " + vehiculo);
-        this.nombreUsuario.setBounds(10, 10, 200, 20);
-        this.dificultad.setBounds(10, 30, 200, 20);
-        this.vehiculoActual.setBounds(10, 50, 200, 20);
-        this.botonGuardar.setBounds(10, 550, 200, 30);
-        this.botonVolver.setBounds(10, 600, 200, 30);
-        this.nota.setBounds(10, 750, 500, 20);
+        this.botonGuardar.setAlignmentX(CENTER_ALIGNMENT);
+        this.botonVolver.setAlignmentX(CENTER_ALIGNMENT);
 
-        this.add(nota);
-        this.add(this.nombreUsuario);
-        this.add(this.dificultad);
-        this.add(this.vehiculoActual);
-        this.add(botonGuardar);
-        this.add(botonVolver);
+        this.panelInfo.add(Box.createVerticalStrut(10));
+        this.panelInfo.add(this.nombreUsuario);
+        this.panelInfo.add(Box.createVerticalStrut(10));
+        this.panelInfo.add(this.dificultad);
+        this.panelInfo.add(Box.createVerticalStrut(10));
+        this.panelInfo.add(this.vehiculoActual);
+        this.panelInfo.add(Box.createVerticalStrut(50));
+        this.panelInfo.add(botonGuardar);
+        this.panelInfo.add(Box.createVerticalStrut(20));
+        this.panelInfo.add(botonVolver);
+
+        
+        this.add(panelInfo);
+        this.add(Box.createHorizontalStrut(20));
         this.add(panelZonaDelJuego);
     }
 
