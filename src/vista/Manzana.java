be7162a2d22038f7d1bcb6f5b4e9d3c79ef2fa53
cabrerajjorge <cabrerajjorge@9d81dd44-56ad-d58c-10f6-30d8cb2e93ac>
2013-12-10 16:12:29 +0000

    JorgeCabrera: subo un método para cambiar la imagen del vehiculo lo dejo comentado porque hay q resolver unos bugs

--- src/vista/Manzana.java
+++ src/vista/Manzana.java
@@ -3,6 +3,7 @@ package vista;
 public class Manzana extends VistaEventoColicion {
     static int longitud = 20;
     static String direccion = "src/vista/imagenes/manzana.png";
+    
 
     public Manzana(int posX, int posY) {
         super(direccion, posX, posY, longitud, longitud);
