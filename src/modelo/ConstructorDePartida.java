b7f918b8315b9979530208bc48eb2f2839a6dfda
nexodrone <nexodrone@gmail.com>
2013-12-12 13:57:14 +0000

    En el informe va todo lo que hicimos hasta ahora: lo mio, lo de Jorge, lo de Manu y Cris.

--- src/modelo/ConstructorDePartida.java
+++ src/modelo/ConstructorDePartida.java
@@ -14,6 +14,7 @@ public class ConstructorDePartida {
 	public static Partida construirPartidaCon4x4(Nivel nivel){
 		Posicion posicionGanadora = generarPosicionValida(nivel);
 		Posicion posicionInicial = generarPosicionValida(nivel);
+		while (posicionGanadora.equals(posicionInicial)) { posicionInicial = generarPosicionValida(nivel); }
 		
 		Vehiculo camioneta = new Vehiculo4x4(posicionInicial);
 		Partida partida = new Partida(nivel.tablero, camioneta, posicionGanadora, nivel.movimientosDisponibles);
@@ -25,6 +26,7 @@ public class ConstructorDePartida {
 	public static Partida construirPartidaConMoto(Nivel nivel){
 		Posicion posicionGanadora = generarPosicionValida(nivel);
 		Posicion posicionInicial = generarPosicionValida(nivel);
+		while (posicionGanadora.equals(posicionInicial)) { posicionInicial = generarPosicionValida(nivel); }
 		
 		Vehiculo moto = new VehiculoMoto(posicionInicial);
 		Partida partida = new Partida(nivel.tablero, moto, posicionGanadora, nivel.movimientosDisponibles);
@@ -36,6 +38,7 @@ public class ConstructorDePartida {
 	public static Partida construirPartidaConAuto(Nivel nivel){
 		Posicion posicionGanadora = generarPosicionValida(nivel);
 		Posicion posicionInicial = generarPosicionValida(nivel);
+		while (posicionGanadora.equals(posicionInicial)) { posicionInicial = generarPosicionValida(nivel); }
 		
 		Vehiculo auto = new VehiculoAuto(posicionInicial);
 		Partida partida = new Partida(nivel.tablero, auto, posicionGanadora, nivel.movimientosDisponibles);
